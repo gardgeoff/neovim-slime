170e86969211b29f5c82d95c3cd89c91482c3228
Geoff Gard <gardgeoff@gmail.com>
2025-08-16 12:41:59 -0700

    update telescope title color contrast

--- lua/slime/plugins/telescope.lua
+++ lua/slime/plugins/telescope.lua
@@ -3,66 +3,66 @@ local utils = require("slime.utils")
 local M = {}
 
 function M.setup(colors, config)
-    -- Only setup if telescope is loaded
-    if not utils.plugin_loaded("telescope") and not utils.plugin_loaded("telescope.nvim") then
-        return
-    end
+	-- Only setup if telescope is loaded
+	if not utils.plugin_loaded("telescope") and not utils.plugin_loaded("telescope.nvim") then
+		return
+	end
 
-    utils.highlight("TelescopeBorder", {
-        fg = colors.telescope_bg,
-        bg = colors.telescope_bg
-    }, config)
-    utils.highlight("TelescopePromptBorder", {
-        fg = colors.telescope_prompt_bg,
-        bg = colors.telescope_prompt_bg
-    }, config)
-    utils.highlight("TelescopeResultsBorder", {
-        fg = colors.telescope_bg,
-        bg = colors.telescope_bg
-    }, config)
-    utils.highlight("TelescopePreviewBorder", {
-        fg = colors.telescope_bg,
-        bg = colors.telescope_bg
-    }, config)
+	utils.highlight("TelescopeBorder", {
+		fg = colors.telescope_bg,
+		bg = colors.telescope_bg,
+	}, config)
+	utils.highlight("TelescopePromptBorder", {
+		fg = colors.telescope_prompt_bg,
+		bg = colors.telescope_prompt_bg,
+	}, config)
+	utils.highlight("TelescopeResultsBorder", {
+		fg = colors.telescope_bg,
+		bg = colors.telescope_bg,
+	}, config)
+	utils.highlight("TelescopePreviewBorder", {
+		fg = colors.telescope_bg,
+		bg = colors.telescope_bg,
+	}, config)
 
-    utils.highlight("TelescopePromptNormal", {
-        fg = colors.fg,
-        bg = colors.telescope_prompt_bg
-    }, config)
-    utils.highlight("TelescopeResultsNormal", {
-        fg = colors.fg,
-        bg = colors.telescope_bg
-    }, config)
-    utils.highlight("TelescopePreviewNormal", {
-        fg = colors.fg,
-        bg = colors.telescope_bg
-    }, config)
+	utils.highlight("TelescopePromptNormal", {
+		fg = colors.fg,
+		bg = colors.telescope_prompt_bg,
+	}, config)
+	utils.highlight("TelescopeResultsNormal", {
+		fg = colors.fg,
+		bg = colors.telescope_bg,
+	}, config)
+	utils.highlight("TelescopePreviewNormal", {
+		fg = colors.fg,
+		bg = colors.telescope_bg,
+	}, config)
 
-    utils.highlight("TelescopePromptTitle", {
-        fg = colors.fg,
-        bg = colors.keyword,
-        bold = true
-    }, config)
-    utils.highlight("TelescopeResultsTitle", {
-        fg = colors.fg,
-        bg = colors.keyword,
-        bold = true
-    }, config)
-    utils.highlight("TelescopePreviewTitle", {
-        fg = colors.fg,
-        bg = colors.keyword,
-        bold = true
-    }, config)
+	utils.highlight("TelescopePromptTitle", {
+		fg = colors.fg,
+		bg = colors.declaration,
+		bold = true,
+	}, config)
+	utils.highlight("TelescopeResultsTitle", {
+		fg = colors.fg,
+		bg = colors.declaration,
+		bold = true,
+	}, config)
+	utils.highlight("TelescopePreviewTitle", {
+		fg = colors.fg,
+		bg = colors.declaration,
+		bold = true,
+	}, config)
 
-    utils.highlight("TelescopeMatching", {
-        fg = colors.keyword,
-        bold = true
-    }, config)
-    utils.highlight("TelescopeSelection", {
-        fg = colors.fg,
-        bg = colors.telescope_selection_bg,
-        bold = true
-    }, config)
+	utils.highlight("TelescopeMatching", {
+		fg = colors.keyword,
+		bold = true,
+	}, config)
+	utils.highlight("TelescopeSelection", {
+		fg = colors.fg,
+		bg = colors.telescope_selection_bg,
+		bold = true,
+	}, config)
 end
 
 return M

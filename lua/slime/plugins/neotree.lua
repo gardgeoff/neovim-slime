b195169121783b54930bd9a3fa6f57a97fe0d378
Geoff Gard <gardgeoff@gmail.com>
2025-08-15 18:34:43 -0700

    update neotree folder color

--- lua/slime/plugins/neotree.lua
+++ lua/slime/plugins/neotree.lua
@@ -3,72 +3,72 @@ local utils = require("slime.utils")
 local M = {}
 
 function M.setup(colors, config)
-    -- Only setup if neo-tree is loaded
-    if not utils.plugin_loaded("neo-tree") and not utils.plugin_loaded("neo-tree.nvim") then
-        return
-    end
+	-- Only setup if neo-tree is loaded
+	if not utils.plugin_loaded("neo-tree") and not utils.plugin_loaded("neo-tree.nvim") then
+		return
+	end
 
-    utils.highlight("NeoTreeDirectoryName", {
-        fg = colors.declaration,
-        bg = colors.bg,
-        bold = true
-    }, config)
-    utils.highlight("NeoTreeDirectoryIcon", {
-        fg = colors.declaration,
-        bg = colors.bg
-    }, config)
-    utils.highlight("NeoTreeWinSeparator", {
-        fg = colors.line,
-        bg = colors.bg
-    }, config)
-    utils.highlight("Directory", {
-        fg = colors.declaration,
-        bg = colors.bg
-    }, config)
+	utils.highlight("NeoTreeDirectoryName", {
+		fg = colors.fg,
+		bg = colors.bg,
+		bold = true,
+	}, config)
+	utils.highlight("NeoTreeDirectoryIcon", {
+		fg = colors.declaration,
+		bg = colors.bg,
+	}, config)
+	utils.highlight("NeoTreeWinSeparator", {
+		fg = colors.line,
+		bg = colors.bg,
+	}, config)
+	utils.highlight("Directory", {
+		fg = colors.fg,
+		bg = colors.bg,
+	}, config)
 
-    -- Additional neo-tree highlights
-    utils.highlight("NeoTreeNormal", {
-        fg = colors.fg,
-        bg = colors.bg
-    }, config)
-    utils.highlight("NeoTreeNormalNC", {
-        fg = colors.fg,
-        bg = colors.bg
-    }, config)
-    utils.highlight("NeoTreeRootName", {
-        fg = colors.declaration,
-        bold = true
-    }, config)
-    utils.highlight("NeoTreeFileName", {
-        fg = colors.fg
-    }, config)
-    utils.highlight("NeoTreeFileIcon", {
-        fg = colors.accent3
-    }, config)
-    utils.highlight("NeoTreeModified", {
-        fg = colors.git_change
-    }, config)
-    utils.highlight("NeoTreeGitAdded", {
-        fg = colors.git_add
-    }, config)
-    utils.highlight("NeoTreeGitDeleted", {
-        fg = colors.git_delete
-    }, config)
-    utils.highlight("NeoTreeGitModified", {
-        fg = colors.git_change
-    }, config)
-    utils.highlight("NeoTreeGitConflict", {
-        fg = colors.error
-    }, config)
-    utils.highlight("NeoTreeGitUntracked", {
-        fg = colors.comment
-    }, config)
-    utils.highlight("NeoTreeIndentMarker", {
-        fg = colors.line
-    }, config)
-    utils.highlight("NeoTreeExpander", {
-        fg = colors.comment
-    }, config)
+	-- Additional neo-tree highlights
+	utils.highlight("NeoTreeNormal", {
+		fg = colors.fg,
+		bg = colors.bg,
+	}, config)
+	utils.highlight("NeoTreeNormalNC", {
+		fg = colors.fg,
+		bg = colors.bg,
+	}, config)
+	utils.highlight("NeoTreeRootName", {
+		fg = colors.declaration,
+		bold = true,
+	}, config)
+	utils.highlight("NeoTreeFileName", {
+		fg = colors.fg,
+	}, config)
+	utils.highlight("NeoTreeFileIcon", {
+		fg = colors.accent3,
+	}, config)
+	utils.highlight("NeoTreeModified", {
+		fg = colors.git_change,
+	}, config)
+	utils.highlight("NeoTreeGitAdded", {
+		fg = colors.git_add,
+	}, config)
+	utils.highlight("NeoTreeGitDeleted", {
+		fg = colors.git_delete,
+	}, config)
+	utils.highlight("NeoTreeGitModified", {
+		fg = colors.git_change,
+	}, config)
+	utils.highlight("NeoTreeGitConflict", {
+		fg = colors.error,
+	}, config)
+	utils.highlight("NeoTreeGitUntracked", {
+		fg = colors.comment,
+	}, config)
+	utils.highlight("NeoTreeIndentMarker", {
+		fg = colors.line,
+	}, config)
+	utils.highlight("NeoTreeExpander", {
+		fg = colors.comment,
+	}, config)
 end
 
 return M
